4edf04915d740ac11d81ea46366116f69b09fbdb
aravind987 <50051060+aravind987@users.noreply.github.com>
2021-12-28 16:08:31 -0500

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+
+function navbar(){
+    return (
+        <>
+            <nav className='navbar'>
+                
+            </nav>
+        </>
+    )
+}
+
+export default navbar;
