dacc213dc0305243a194cec39af44567576e94bd
aravind987 <50051060+aravind987@users.noreply.github.com>
2021-12-28 16:01:25 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import navbar from './components/navbar' 
+import About from './components/pages/About';
+import Home from './components/pages/Home'
+
+function App() {
+  return (
+    <navbar />
+  );
+}
+
+export default App;
