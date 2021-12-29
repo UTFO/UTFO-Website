d9014718e14ba2ec3ec4b7973ad50ad39ed23749
AbyxDev <ken@abyx.dev>
2021-12-29 00:16:23 -0500

    Add proper routing
    This also involves making a VERY barebones navlist just to test it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from './components/pages/About';
-import Home from './components/pages/Home';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 function Navbar() {
-    return (
-        <>
-            <nav className='navbar'>
-
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className='navbar'>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
