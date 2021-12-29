a67f71589dfc5c0fcdda564b38f39e5ef0aed6bd
AbyxDev <ken@abyx.dev>
2021-12-28 22:56:41 -0500

    Make into proper Node package

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+function navbar(){
+    return (
+        <>
+            <nav className='navbar'>
+
+            </nav>
+        </>
+    )
+}
+
+export default navbar;
