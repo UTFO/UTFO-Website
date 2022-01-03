154ba7cf4b93e55f3bb85778303cda87158ad991
Tyler Wong <tylerwong7555@gmail.com>
2022-01-03 00:01:40 -0500

    Updated Navbar and index
    Deleted unnecessary React assets
    Added temporary css for navbar
    Made responsive collapsible navbar design
    Improved imports in index.js for pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
+import { Home, About } from './components/pages/imports';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -13,7 +12,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
