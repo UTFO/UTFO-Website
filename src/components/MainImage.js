908f887c528ec0c1bc3e6588dee42ada70993ca3
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-01-05 17:35:37 -0500

    Created header, main image, and footer for front page

---
+++ src/components/MainImage.js
@@ -0,0 +1,33 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import mainImage from './main-image.svg';
+import './MainImage.css'
+
+function MainImage() {
+  
+  const scrollToNext = () => { 
+    if (document.documentElement.scrollTop <= 0) {
+      // element which needs to be scrolled to
+      const element = document.querySelector(".footer");
+
+      // scroll to element
+      element.scrollIntoView({behavior: 'smooth'});
+    }
+  };  
+      
+  return (
+      <div className='main-image'>
+          <img src={mainImage} alt='main' />
+          <div className='title-wrapper' >
+              <h1>University of Toronto Freelancer's Organization</h1>
+          </div>
+          <div className='scrolldown-button-wrapper'>
+              <button onClick={scrollToNext} >
+                  {<KeyboardArrowDownIcon />}
+                  {/* [Arrow] */}
+              </button>
+          </div>
+      </div>
+  );
+}
+
+export default MainImage;
