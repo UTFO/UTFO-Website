2130f72307e1623514e4a2dbbcdf91def20af1db
ozzy <o.sultan2811@gmail.com>
2022-01-08 18:03:45 -0500

    imported styling, should work now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/pages/Navbar/Navbar';
+import './App.css';
 
 function App() {
   return (
