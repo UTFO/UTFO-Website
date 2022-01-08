7bd96d20ad319df09b8396a6a431e3d4954a17bd
ozzy <o.sultan2811@gmail.com>
2022-01-08 17:37:48 -0500

    cleaned up some files, moved navbar to pages

--- src/components/pages/Home/Home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '.';
-
-test('renders blurb', () => {
-  render(<Home />);
-  const blurbElement = screen.getByText(/UTFO will not charge/);
-  expect(blurbElement).toBeInTheDocument();
-});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import { Home, About, Article, Contact } from './components/pages/imports';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,7 +20,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
