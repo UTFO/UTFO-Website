374971e14dc2e59363cfe120aa0d7ed762538682
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-01-09 18:25:47 -0500

    Minor edits and parent containers for blurbs

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -3,11 +3,43 @@ import MainImage from './MainImage'
 import './Home.css';
 
 function Home() {
+
+  const reveal = () => {
+    const reveals = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = reveals[i].getBoundingClientRect().top;
+      const elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } 
+      else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
   return (
-    <>
-      <MainImage />
-      <Blurb />
-    </>
+    <div className='home'>
+      <>
+        <MainImage />
+        <div className='reveal accent' id='first-blurb'>
+          <Blurb />
+        </div>
+        <div className='reveal'>
+          {/* Blurb for article page */}
+          <Blurb />
+        </div>
+        <div className='reveal accent'>
+          {/* Blurb for contact page */}
+          <Blurb />
+        </div>
+      </>
+    </div>
   );
 }
 
