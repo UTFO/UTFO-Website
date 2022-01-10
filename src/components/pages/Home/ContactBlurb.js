5e5388225d59bce16660ed3ea4b5cf9ded7fc9ef
Autumn-Coding <78398232+Autumn-Coding@users.noreply.github.com>
2022-01-10 17:02:08 -0500

    Update ContactBlurb.js

--- src/components/pages/Home/ContactBlurb.js
+++ src/components/pages/Home/ContactBlurb.js
@@ -3,7 +3,9 @@ function ContactBlurb() {
 		<>
 			<h2>Contact Us</h2>
 			<p className="blurb">
-				lorem ipsum
+				If you have any questions, concerns, suggestions, or just want some more 
+				information about what we do, drop us an email using our form here and 
+				you can expect a response within 3 to 5 business days.
 			</p>
 		</>
 	);
