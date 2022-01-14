ba8b71750346ab718e2a47cd9d756915b4aae2f6
AbyxDev <ken@abyx.dev>
2022-01-13 23:57:34 -0500

    Add social components and a bunch of new roles

--- src/components/pages/About/Socials.js
+++ src/components/pages/About/Socials.js
@@ -23,6 +23,18 @@ export function Social(props) {
 }
 
 /**
+ * Format a personal website link.
+ * @param {{
+ * children: string
+ * }} props The website URL to link to.
+ */
+export function Website(props) {
+	return (
+		<Social url={props.children} platform="Website" fab="fas fa-link" />
+	);
+}
+
+/**
  * Format a GitHub link.
  * @param {{
  * children: string
@@ -46,8 +58,25 @@ export function LinkedIn(props) {
 	);
 }
 
+/**
+ * Format an Instagram link.
+ * @param {{
+ * children: string
+ * }} props The Instagram username to link to.
+ */
+export function Instagram(props) {
+	return (
+		<Social url={'https://instagram.com/' + props.children} platform="Instagram" />
+	);
+}
+
 export const Roles = {
 	founder: (<div><i className="far fa-lightbulb"></i> <span>Founder</span></div>),
 	chiefExec: (<div><i className="fas fa-crown"></i> <span>Chief Executive</span></div>),
-	frontend: (<div><i className="fas fa-code"></i> <span>Front-end Developer</span></div>)
+	vp: (<div><i className="fas fa-crown"></i> <span>Vice President</span></div>),
+	frontend: (<div><i className="fas fa-code"></i> <span>Front-end Developer</span></div>),
+	backend: (<div><i className="fas fa-server"></i> <span>Back-end Developer</span></div>),
+	mobile: (<div><i className="fas fa-mobile"></i> <span>Mobile Developer</span></div>),
+	treasurer: (<div><i className="fas fa-dollar-sign"></i> <span>Treasurer</span></div>),
+	secretary: (<div><i className="fas fa-pencil-alt"></i> <span>Secretary</span></div>)
 };
