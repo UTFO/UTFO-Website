b4219e2f99d594fb927d2ef4962e96cc1f8c29fb
AbyxDev <ken@abyx.dev>
2022-01-17 01:30:55 -0500

    Align MainImage correctly

--- src/components/pages/Home/MainImage.js
+++ src/components/pages/Home/MainImage.js
@@ -7,8 +7,8 @@ function MainImage() {
   return (
     <div className='main-image inner'>
       <img src={mainImage} alt='main' />
-      <div className='title-wrapper' >
-        <h1>
+      <div className='text' >
+        <h1 className='title-wrapper'>
           <p className="uoft">University of Toronto</p>
           <p className="fo">Freelancer's<br/>Organization</p>
         </h1>
