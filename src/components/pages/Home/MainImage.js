0af3a6d6ce11f958bb3afce5448fb28fb817c42c
AbyxDev <ken@abyx.dev>
2022-01-17 01:19:16 -0500

    Redesign MainImage

--- src/components/pages/Home/MainImage.js
+++ src/components/pages/Home/MainImage.js
@@ -2,14 +2,17 @@ import mainImage from './../../../assets/main-image.svg';
 import './MainImage.css'
 
 function MainImage() {
-     
+
   return (
     <div className='main-image inner'>
-          <img src={mainImage} alt='main' />
-          <div className='title-wrapper' >
-              <h1>University of Toronto Freelancer's Organization</h1>
-          </div>
+      <img src={mainImage} alt='main' />
+      <div className='title-wrapper' >
+        <h1>
+          <p className="uoft">University of Toronto</p>
+          <p className="fo">Freelancer's<br/>Organization</p>
+        </h1>
       </div>
+    </div>
   );
 }
 
