a4909fe8b7d6d75980071b42b1ef3317452211e5
AbyxDev <ken@abyx.dev>
2022-02-04 18:23:16 -0500

    Clean up Contact page

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -23,25 +23,25 @@ class App extends React.Component {
   // render() {
   //   return (
   //     <form onSubmit={this.handleSubmit} className="form">
-  //       <div className='contact-title'>SEND US A MESSAGE!</div> 
-        
+  //       <div className='contact-title'>SEND US A MESSAGE!</div>
+
   //       <input type="text" placeholder="Name" />
 
   //       <input type="email" placeholder="Email" value={this.state.value} onChange={this.handleChange} />
 
-  //       <textarea placeholder="Write your message here..." rows='6' />      
+  //       <textarea placeholder="Write your message here..." rows='6' />
 
   //       <input type="submit" value="Submit" />
-        
+
   //     </form>
   //   );
   // }
   render() {
     return (
-      <div className='form-container'>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSegAj-XyKDQT5x0AfKDoVap2mJKhG5cXl_HUBZe3eSo_k1N7A/viewform?embedded=true" height='1026'>Loading…</iframe>
+      <div className='contact-form inner'>
+        <iframe title="Contact form" src="https://docs.google.com/forms/d/e/1FAIpQLSegAj-XyKDQT5x0AfKDoVap2mJKhG5cXl_HUBZe3eSo_k1N7A/viewform?embedded=true">Loading…</iframe>
         <div className='information'>
-          <text>Get in touch with us here</text>
+          <h2>Get in touch with us here</h2>
           <img src={Logo} alt='spearman logo'></img>
         </div>
       </div>
