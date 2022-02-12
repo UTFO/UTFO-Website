ca0d1953127a6110c6f9256d582f2a7379139cf6
AbyxDev <ken@abyx.dev>
2022-02-11 22:10:00 -0500

    Add social links to Contact page
    Closes #7

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -43,6 +43,12 @@ class App extends React.Component {
         <div className='information'>
           <h2>Get in touch with us here</h2>
           <img src={Logo} alt='spearman logo'></img>
+          <div>
+            <a href="https://twitter.com"><i className="fab fa-twitter"></i></a>
+            <a href="https://instagram.com"><i className="fab fa-instagram"></i></a>
+            <a href="https://facebook.com"><i className="fab fa-facebook"></i></a>
+            <a href="https://www.linkedin.com/company/university-of-toronto-freelancers-organization-utfo/"><i className="fab fa-linkedin"></i></a>
+          </div>
         </div>
       </div>
     );
