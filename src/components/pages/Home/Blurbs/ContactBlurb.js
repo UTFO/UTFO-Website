c0f4f092a1c4d240d0f2035c5ad7784a832cacf8
AbyxDev <ken@abyx.dev>
2022-02-11 22:26:33 -0500

    Use entire contact page as contact section
    Just use some clever styling to make it fit
    
    Closes #10

--- src/components/pages/Home/Blurbs/ContactBlurb.js
+++ src/components/pages/Home/Blurbs/ContactBlurb.js
@@ -1,14 +1,4 @@
-function ContactBlurb() {
-	return (
-		<>
-			<h2>Contact Us</h2>
-			<p className="blurb">
-				If you have any questions, concerns, suggestions, or just want some more
-				information about what we do, drop us an email using our form here and
-				you can expect a response within 3 to 5 business days.
-			</p>
-		</>
-	);
-}
+import Contact from "../../Contact";
+import "./ContactBlurb.css";
 
-export default ContactBlurb;
+export default Contact;
