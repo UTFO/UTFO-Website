15ea40f0213078b51ee2f672509aaf19e38391c4
tyler-jon-wong <tylerwong7555@gmail.com>
2022-03-30 17:58:34 -0400

    Changed axios to check production build

--- src/components/pages/api.js
+++ src/components/pages/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "",
+  baseURL: process.env.NODE_ENV === "production" ? "" : "http://localhost:8080",
 });
 
 // Get all article data to show on main article page
@@ -12,11 +12,11 @@ export const getFullArticle = (id) => API.get(`/api/v1/fullArticle/${id}`);
 
 // Create new article
 export const createArticle = (newArticle) =>
-    API.post("api/v1/fullArticle", newArticle);
+  API.post("api/v1/fullArticle", newArticle);
 
 // Update an article
 export const updateArticle = (id, article) =>
-    API.patch(`/api/v1/fullArticle/${id}`, article);
+  API.patch(`/api/v1/fullArticle/${id}`, article);
 
 export const deleteArticlePermanently = (id) =>
-    API.delete(`/api/v1/fullArticle/${id}`);
+  API.delete(`/api/v1/fullArticle/${id}`);
