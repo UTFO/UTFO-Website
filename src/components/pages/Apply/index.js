87b9fb73ad2e7cc2438bef333838d2c750307c2d
brettyang003 <brettyang78@gmail.com>
2022-04-06 15:20:02 -0400

    Made apply page responsive

--- src/components/pages/Apply/index.js
+++ src/components/pages/Apply/index.js
@@ -1,10 +1,10 @@
-import './Apply.css';
-import WaveDivider from '../../../assets/wave-haikei.svg';
-import Logo from '../../../assets/logo_transparent.png';
-import Arrow from '../../../assets/long-arrow-right.png';
-import Developer from '../../../assets/developer.png';
-import Designer from '../../../assets/designer.png';
-import ContentWriter from '../../../assets/contentwriter.png';
+import "./Apply.css";
+import WaveDivider from "../../../assets/wave-haikei.svg";
+import Logo from "../../../assets/logo_transparent.png";
+import Arrow from "../../../assets/long-arrow-right.png";
+import Developer from "../../../assets/developer.png";
+import Designer from "../../../assets/designer.png";
+import ContentWriter from "../../../assets/contentwriter.png";
 
 /**
  * Format a role.
@@ -23,15 +23,22 @@ function Role(props) {
     </div>
   );
   let img = (
-    <img
-      className={props.right ? "right" : ""}
-      src={props.img} alt=""
-    />
+    <img className={props.right ? "right img" : "img"} src={props.img} alt="" />
   );
   if (props.right) {
-    return <>{text}{img}</>;
+    return (
+      <>
+        {text}
+        {img}
+      </>
+    );
   } else {
-    return <>{img}{text}</>;
+    return (
+      <>
+        {img}
+        {text}
+      </>
+    );
   }
 }
 
@@ -42,13 +49,16 @@ function App() {
         <div className="inner">
           <h1>Looking to get involved?</h1>
           <p>
-            Do you want to be part of a fast, dynamic and friendly team? 
-            Do you want to gain hands on development experience working for real clients?
-            Do you want to get a glimpse of what development actually feels like?
-            Are you someone that can understand what a client would want, and deliver exactly that?
-            If you answered "Yes" to these questions, UTFO is definitely the right place for you.
-            We will constantly increase the number and types of projects we do, taking on harder and bigger challenges in the process.
-            Fill the application form at the bottom of this page, and come join us for all of this and beyond.
+            Do you want to be part of a fast, dynamic and friendly team? Do you
+            want to gain hands on development experience working for real
+            clients? Do you want to get a glimpse of what development actually
+            feels like? Are you someone that can understand what a client would
+            want, and deliver exactly that? If you answered "Yes" to these
+            questions, UTFO is definitely the right place for you. We will
+            constantly increase the number and types of projects we do, taking
+            on harder and bigger challenges in the process. Fill the application
+            form at the bottom of this page, and come join us for all of this
+            and beyond.
           </p>
         </div>
       </div>
@@ -68,34 +78,32 @@ function App() {
             Specific types include Software, Security, Mobile, Game Developer,
             Data Scientist, DevOps, Frontend, and Backend.
           </Role>
-          <div className="role"></div>
           <Role name="Designers" img={Designer} right={true}>
             The artists who create designs for the developers to bring to life.
             They make the website layouts, the logos (?), the etc etc...
           </Role>
           <Role name="Content Writers" img={ContentWriter}>
-            The writers that bring out the best of the clients.
-            They are in charge of the text that users see and out UTFO articles!
+            The writers that bring out the best of the clients. They are in
+            charge of the text that users see and out UTFO articles!
           </Role>
         </div>
         <div className="dash">-</div>
-        <div className='join inner'>
+        <div className="join inner role">
           <h1>Become a Member!</h1>
-            <p>
-              Sign up through the form below to become a member of the University
-              of Toronto Freelancers' Organization! Further join details will
-              be sent to your email. Our main platform of communication is
-              Discord, which you will be invited to upon completion of this form.
-            </p>
-            <br></br>
-            <a href = "https://docs.google.com/forms/d/e/1FAIpQLSeglrUFoM2vh7vWzHYHqtAMIeOKqKvKMVA4LGeR0wnR4nXDXA/viewform">
-                CLICK HERE FOR THE APPLICATION FORM
-            </a>
+          <p>
+            Sign up through the form below to become a member of the University
+            of Toronto Freelancers' Organization! Further join details will be
+            sent to your email. Our main platform of communication is Discord,
+            which you will be invited to upon completion of this form.
+          </p>
+          <br></br>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSeglrUFoM2vh7vWzHYHqtAMIeOKqKvKMVA4LGeR0wnR4nXDXA/viewform">
+            CLICK HERE FOR THE APPLICATION FORM
+          </a>
         </div>
       </div>
     </div>
   );
 }
-
 
 export default App;
